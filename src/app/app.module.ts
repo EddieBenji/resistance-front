8aee623fe77cb566abd069c4cbda185abf097431
Lalo <eduardo.942712@gmail.com>
2020-04-07 10:34:09 -0500

    - FROM THIS STEP, YOU CAN CLONE IT FOR HAVING DEFAULT CONFIGURATIONS. - added ngrx to the entire project.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,13 @@ import { AngularMaterialModule } from './shared/modules/angular-material.module'
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './reducers';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { EffectsModule } from '@ngrx/effects';
+import { AppEffects } from './reducers/app.effects';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
 
 @NgModule({
     declarations: [
@@ -18,7 +25,13 @@ import { HomeComponent } from './home/home.component';
         BrowserModule,
         AppRoutingModule,
         BrowserAnimationsModule,
-        AngularMaterialModule
+        AngularMaterialModule,
+        StoreModule.forRoot(reducers, {
+            metaReducers
+        }),
+        StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+        EffectsModule.forRoot([ AppEffects ]),
+        StoreRouterConnectingModule.forRoot()
     ],
     providers: [],
     bootstrap: [ AppComponent ]
